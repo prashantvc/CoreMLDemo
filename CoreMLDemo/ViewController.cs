500d078b554f2fd1a8e42ae52a50c5e624177106
Prashant Cholachagudda <prchol@microsoft.com>
2018-02-15 17:44:54 +0530

    Add project files.

---
+++ CoreMLDemo/ViewController.cs
@@ -0,0 +1,96 @@
+﻿using CoreML;
+using Foundation;
+using Plugin.Media;
+using System;
+using System.Diagnostics;
+using UIKit;
+using Vision;
+
+namespace CoreMLDemo
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            btnPicture.TouchUpInside += BtnPicture_TouchUpInside;
+        }
+
+        async void BtnPicture_TouchUpInside(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                Debug.WriteLine("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+            Debug.WriteLine("File Location", file.Path, "OK");
+
+            var imagedata = NSData.FromStream(file.GetStream());
+            capturedImage.Image =
+                UIImage.LoadFromData(imagedata);
+
+            var requestHandler = new VNImageRequestHandler(imagedata, new VNImageOptions());
+            requestHandler.Perform(ClassificationRequest, out NSError error);
+
+            if (error != null)
+                Debug.WriteLine($"Error identifying {error}");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public VNRequest[] ClassificationRequest
+        {
+            get
+            {
+                if (model == null)
+                {
+                    var modelPath = NSBundle.MainBundle.GetUrlForResource("Link", "mlmodel");
+                    var compiledPath = MLModel.CompileModel(modelPath, out NSError compileError);
+                    var mlModel = MLModel.Create(compiledPath, out NSError createError);
+
+                    model = VNCoreMLModel.FromMLModel(mlModel, out NSError mlError);
+                }
+
+                if (classificationRequests == null)
+                {
+                    var classificationRequest = new VNCoreMLRequest(model, HandleClassificationRequest);
+                    classificationRequests = new[] { classificationRequest };
+                }
+
+                return classificationRequests;
+            }
+        }
+
+        void HandleClassificationRequest(VNRequest request, NSError error)
+        {
+            var observations = request.GetResults<VNClassificationObservation>();
+            var best = observations?[0];
+
+            outputLabel.Text = $"{best.Identifier.Trim()}, {best.Confidence:P0}";
+        }
+
+        VNCoreMLModel model;
+        VNRequest[] classificationRequests;
+    }
+}
---
+++ CoreMLDemo/ViewController.designer.cs
@@ -0,0 +1,55 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace CoreMLDemo
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnPicture { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView capturedImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel outputLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIStackView stackLayout { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnPicture != null) {
+                btnPicture.Dispose ();
+                btnPicture = null;
+            }
+
+            if (capturedImage != null) {
+                capturedImage.Dispose ();
+                capturedImage = null;
+            }
+
+            if (outputLabel != null) {
+                outputLabel.Dispose ();
+                outputLabel = null;
+            }
+
+            if (stackLayout != null) {
+                stackLayout.Dispose ();
+                stackLayout = null;
+            }
+        }
+    }
+}
